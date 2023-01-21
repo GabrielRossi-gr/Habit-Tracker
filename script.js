aa6cdfc42dd730387d1d8dcf946cdc5a20ddf87c
GabrielRossi <gabrieldesouzarossi123456@gmail.com>
2023-01-21 12:02:56 -0300

    12/01/2023 0

--- script.js
+++ script.js
@@ -4,18 +4,30 @@ const nlwSetup = new NLWSetup(form)
 
 const button = document.querySelector('header button')
 button.addEventListener('click', add)
+form.addEventListener('change', save)
+
+
+
 function add(){
-    alert('alerta')
+    const toDay = new Date().toLocaleDateString('pt-br').slice(0, 5)
+
+    const DaiExist = nlwSetup.dayExists(toDay)
+    if(DaiExist){    
+        alert('the day already exist 😒')
+    }else{
+        nlwSetup.addDay(toDay)
+    }
 }
 
-/*
-const data = {
-    run: ["01-16","01-17","01-18","01-19", "01-20", "01-21", "01-22", "01-23", "01-24", "01-25", "01-26", "01-27", "01-28", "01-29", "01-30", "02-01", "02-02", "02-03", "02-04", "02-05", "02-06"],
-    water: ["01-17", "01-18", "01-19"],
+function save(){
+ 
+    localStorage.setItem('NLWsetup@Habits', JSON.stringify(nlwSetup.data) )
 }
 
 
+const data = JSON.parse(localStorage.getItem("NLWsetup@Habits")) || {}
 nlwSetup.setData(data)
 nlwSetup.load()
 
-*/
+
+
